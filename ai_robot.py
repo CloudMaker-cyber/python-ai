8a950a787974476e756f3b02e93a749352a340e3
CloudMaker <amzinglook@outlook.com>
2026-08-22 22:43:16 +0800

    新加页面历史记录展示

--- ai_robot.py
+++ ai_robot.py
@@ -19,7 +19,17 @@ st.title("AI智能伴侣")
 #logo
 st.logo("./resources/touxiang.jpg")
 
-#系统提示此
+#初始化聊天信息
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+#展示聊天信息
+for message in st.session_state.messages:#存储类型：{"role": "user或assistant", "content": prompt}
+    st.chat_message(message["role"]).write(message["content"])
+
+
+
+#系统提示词
 system_prompt = "你是一个历史老师，名字叫张老师，回答问题要严谨专业"
 #创建与ai交互的对象
 client = OpenAI(api_key=os.environ.get('DEEPSEEK_API_KEY'),
@@ -30,6 +40,9 @@ prompt = st.chat_input("请输入你的问题：")
 if prompt:
     st.chat_message("user").write(prompt)
 
+#保存用户输入提示词
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    #调用大模型
     response = client.chat.completions.create(
         model="deepseek-v4-pro",
         messages=[
@@ -43,3 +56,5 @@ if prompt:
 
     print("----------大模型返回结果：",response.choices[0].message.content)
     st.chat_message("assistant").write(response.choices[0].message.content)
+    #保存大模型回复
+    st.session_state.messages.append({"role": "assistant", "content": response.choices[0].message.content})
